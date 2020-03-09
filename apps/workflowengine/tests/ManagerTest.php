027452c895a50adf3c9e528b6de34c6d8fab60d8
tigerater <tigerater@gmail.com>
2020-03-09 18:05:57 +0000

    1138

--- apps/workflowengine/tests/ManagerTest.php
+++ apps/workflowengine/tests/ManagerTest.php
@@ -33,7 +33,6 @@ use OCP\ILogger;
 use OCP\IServerContainer;
 use OCP\IURLGenerator;
 use OCP\IUserSession;
-use OCP\SystemTag\ISystemTagManager;
 use OCP\WorkflowEngine\ICheck;
 use OCP\WorkflowEngine\IEntity;
 use OCP\WorkflowEngine\IManager;
@@ -284,10 +283,7 @@ class ManagerTest extends TestCase {
 							$this->l,
 							$this->createMock(IURLGenerator::class),
 							$this->createMock(IRootFolder::class),
-							$this->createMock(ILogger::class),
-							$this->createMock(\OCP\Share\IManager::class),
-							$this->createMock(IUserSession::class),
-							$this->createMock(ISystemTagManager::class)
+							$this->createMock(ILogger::class)
 						])
 						->setMethodsExcept(['getEvents'])
 						->getMock();
