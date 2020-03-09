4502a460dc5aaa0df71ad105f4471af107365e83
tigerater <tigerater@gmail.com>
2020-03-09 21:13:26 +0000

    300

--- lib/private/Setup.php
+++ lib/private/Setup.php
@@ -353,9 +353,11 @@ class Setup {
 
 		$this->config->setValues($newConfigValues);
 
-		$dbSetup->initialize($options);
 		try {
+			$dbSetup->initialize($options);
 			$dbSetup->setupDatabase($username);
+			// apply necessary migrations
+			$dbSetup->runMigrations();
 		} catch (\OC\DatabaseSetupException $e) {
 			$error[] = [
 				'error' => $e->getMessage(),
@@ -365,16 +367,6 @@ class Setup {
 		} catch (Exception $e) {
 			$error[] = [
 				'error' => 'Error while trying to create admin user: ' . $e->getMessage(),
-				'hint' => '',
-			];
-			return $error;
-		}
-		try {
-			// apply necessary migrations
-			$dbSetup->runMigrations();
-		} catch (Exception $e) {
-			$error[] = [
-				'error' => 'Error while trying to initialise the database: ' . $e->getMessage(),
 				'hint' => '',
 			];
 			return $error;
