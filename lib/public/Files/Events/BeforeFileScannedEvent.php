7cce33d33e96dda3411b87f201d20ef0710dbcaf
tigerater <tigerater@gmail.com>
2020-03-09 19:06:33 +0000

    884

--- lib/public/Files/Events/BeforeFileScannedEvent.php
+++ lib/public/Files/Events/BeforeFileScannedEvent.php
@@ -1,9 +1,11 @@
 <?php
+
 declare(strict_types=1);
+
 /**
- * @copyright Copyright (c) 2019, Roeland Jago Douma <roeland@famdouma.nl>
+ * @copyright 2019 Christoph Wurst <christoph@winzerhof-wurst.at>
  *
- * @author Roeland Jago Douma <roeland@famdouma.nl>
+ * @author 2019 Christoph Wurst <christoph@winzerhof-wurst.at>
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -19,35 +21,36 @@ declare(strict_types=1);
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
-namespace OCP\Share\Events;
+namespace OCP\Files\Events;
 
 use OCP\EventDispatcher\Event;
-use OCP\Share\IShare;
 
 /**
  * @since 18.0.0
  */
-class ShareCreatedEvent extends Event {
+class BeforeFileScannedEvent extends Event {
 
-	/** @var IShare */
-	private $share;
+	/** @var string */
+	private $absolutePath;
 
 	/**
+	 * @param string $absolutePath
+	 *
 	 * @since 18.0.0
 	 */
-	public function __construct(IShare $share) {
+	public function __construct(string $absolutePath) {
 		parent::__construct();
-
-		$this->share = $share;
+		$this->absolutePath = $absolutePath;
 	}
 
 	/**
+	 * @return string
 	 * @since 18.0.0
 	 */
-	public function getShare(): IShare {
-		return $this->share;
+	public function getAbsolutePath(): string {
+		return $this->absolutePath;
 	}
+
 }
