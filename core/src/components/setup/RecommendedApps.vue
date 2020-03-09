946afada35d7a250e6d25e24238ca388b03dc41d
tigerater <tigerater@gmail.com>
2020-03-09 20:45:33 +0000

    442

--- core/src/components/setup/RecommendedApps.vue
+++ core/src/components/setup/RecommendedApps.vue
@@ -82,12 +82,6 @@ const recommended = {
 	talk: {
 		description: t('core', 'Screensharing, online meetings and web conferencing – on desktop and with mobile apps.'),
 	},
-	onlyoffice: {
-		description: t('core', 'Collaboratively edit office documents.'),
-	},
-	documentserver_community: {
-		description: t('core', 'Local document editing back-end used by the OnlyOffice app.'),
-	},
 }
 const recommendedIds = Object.keys(recommended)
 const defaultPageUrl = loadState('core', 'defaultPageUrl')
