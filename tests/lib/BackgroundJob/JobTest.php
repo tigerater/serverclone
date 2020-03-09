67af44c1da74120e338ca0b87c62111b905a9a63
tigerater <tigerater@gmail.com>
2020-03-09 21:02:56 +0000

    353

--- tests/lib/BackgroundJob/JobTest.php
+++ tests/lib/BackgroundJob/JobTest.php
@@ -39,27 +39,6 @@ class JobTest extends \Test\TestCase {
 		$this->assertCount(1, $jobList->getAll());
 	}
 
-	public function testRemoveAfterError() {
-		$jobList = new DummyJobList();
-		$job = new TestJob($this, function () {
-			$test = null;
-			$test->someMethod();
-		});
-		$jobList->add($job);
-
-		$logger = $this->getMockBuilder(ILogger::class)
-			->disableOriginalConstructor()
-			->getMock();
-		$logger->expects($this->once())
-			->method('logException')
-			->with($this->isInstanceOf(\Throwable::class));
-
-		$this->assertCount(1, $jobList->getAll());
-		$job->execute($jobList, $logger);
-		$this->assertTrue($this->run);
-		$this->assertCount(1, $jobList->getAll());
-	}
-
 	public function markRun() {
 		$this->run = true;
 	}
